d88060e76258854dc19142518beff93b9ccd779b
IFearF4ded <tiloualex88@gmail.com>
2025-12-02 22:53:48 -0700

    Delete api/cookies.js

--- api/cookies.js
+++
@@ -1,6 +0,0 @@
-// api/cookies.js
-module.exports = (req, res) => {
-  // Get the raw cookie string from the request headers
-  const cookies = req.headers.cookie || "";
-  res.status(200).json({ receivedCookies: cookies });
-};
